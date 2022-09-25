d1da2a0fcbaf005334f3555c1250030c2f654957
Jari Arkko <jari.arkko@piuha.net>
2022-09-25 18:12:35 +0300

    changed years

--- scripts/pull-all.sh
+++ scripts/pull-all.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-python3 src/pull-data 2015 2021
-python3 src/pull-data --months 01 08 2022
+for y in 2015 2016 2017 2018 2019 2020 2021
+do
+    python3 src/pull-data.py $y
+done
+
+python3 src/pull-data.py --months 01 08 2022
